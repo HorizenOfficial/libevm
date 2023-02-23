4632cdf9031f43dba68b9a852b6d08bddddca37d
Jan Christoph Bernack <jc.bernack@gmail.com>
2023-02-23 14:19:25 +0100

    update build script

--- libevm/build.sh
+++ libevm/build.sh
@@ -11,6 +11,9 @@ go build -buildmode c-shared -o "bin/linux-x86-64/libevm.so"
 # build all test executables
 for i in $(go list ./...); do go test -c -o "bin/linux-x86-64/tests/$i.test" $i; done
 
+echo "# building docker image for cross-compilation to windows"
+docker build -t horizen/libevm:windows-amd64 .
+
 echo "# building for windows"
-docker run --rm -v "$PWD/bin/.cache":/go -v "$PWD":/libevm -w /libevm horizen/libevm:windows-amd64 /bin/sh -c \
+docker run --rm -v "$PWD":/libevm -w /libevm horizen/libevm:windows-amd64 /bin/sh -c \
   "set -ex && go build -buildmode c-shared -o bin/windows-amd64/libevm.dll && for i in \$(go list ./...); do go test -c -o \"bin/windows-amd64/tests/\$i.exe\" \$i; done"
