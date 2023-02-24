470241eb6543076f08bd629824cc8e90811d3b1a
Jan Christoph Bernack <jc.bernack@gmail.com>
2023-02-24 17:17:15 +0100

    remove libevm from maven build

--- libevm/build.sh
+++ libevm/build.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+# delete output folder
+sudo rm -rf bin
+
 # preparation
 go generate ./...
 
@@ -9,15 +12,22 @@ echo "# building for linux"
 # build library
 go build -buildmode c-shared -o "bin/linux-x86-64/libevm.so"
 # build all test executables
-for i in $(go list ./...); do go test -c -o "bin/linux-x86-64/tests/$i.test" $i; done
+#for i in $(go list ./...); do go test -c -o "bin/linux-x86-64/tests/$i.test" $i; done
 
-echo "# building docker image for cross-compilation to windows"
-docker build -t horizen/libevm:win32-x86-64 .
+#echo "# building docker image for cross-compilation to windows"
+#docker build -t horizen/libevm:win32-x86-64 .
 
 echo "# building for windows"
 docker run --rm -v "$PWD":/libevm -w /libevm horizen/libevm:win32-x86-64 /bin/sh -c \
-  "set -ex && go build -buildmode c-shared -o bin/win32-x86-64/libevm.dll && for i in \$(go list ./...); do go test -c -o \"bin/win32-x86-64/tests/\$i.exe\" \$i; done"
+  "go build -buildmode c-shared -o bin/win32-x86-64/libevm.dll"
 
 # dump symbols of shared object
-nm -g bin/linux-x86-64/libevm.so
+#nm -g bin/linux-x86-64/libevm.so
 winedump -j export bin/win32-x86-64/libevm.dll
+
+# change ownership for all directories created by the docker container
+sudo chown -R gigo bin
+
+# move binaries to the EVM package
+cp -r bin/linux-x86-64 ../evm/target/classes/
+cp -r bin/win32-x86-64 ../evm/target/classes/
