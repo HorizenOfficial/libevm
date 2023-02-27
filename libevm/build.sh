9d1416d9e0fef7e3818d3f1cc690403850c9977f
Jan Christoph Bernack <jc.bernack@gmail.com>
2023-02-27 17:33:15 +0100

    cross-compile on host system instead of container

--- libevm/build.sh
+++ libevm/build.sh
@@ -3,30 +3,25 @@
 set -ex
 
 # delete output folder
-sudo rm -rf bin
+rm -rf bin
 
 # preparation
 go generate ./...
 
 echo "# building for linux"
-# build library
 go build -buildmode c-shared -o "bin/linux-x86-64/libevm.so"
-# build all test executables
-#for i in $(go list ./...); do go test -c -o "bin/linux-x86-64/tests/$i.test" $i; done
-
-#echo "# building docker image for cross-compilation to windows"
-#docker build -t horizen/libevm:win32-x86-64 .
 
 echo "# building for windows"
-docker run --rm -v "$PWD":/libevm -w /libevm horizen/libevm:win32-x86-64 /bin/sh -c \
-  "go build -buildmode c-shared -o bin/win32-x86-64/libevm.dll"
+GOOS=windows \
+GOARCH=amd64 \
+CGO_ENABLED=1 \
+CC=x86_64-w64-mingw32-gcc \
+CXX=x86_64-w64-mingw32-g++ \
+go build -buildmode c-shared -o bin/win32-x86-64/libevm.dll
 
 # dump symbols of shared object
 #nm -g bin/linux-x86-64/libevm.so
 winedump -j export bin/win32-x86-64/libevm.dll
-
-# change ownership for all directories created by the docker container
-sudo chown -R gigo bin
 
 # move binaries to the EVM package
 cp -r bin/linux-x86-64 ../evm/target/classes/
