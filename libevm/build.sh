38521add1f69b31d69839d3b8d88b9e85ea6ba31
Jan Christoph Bernack <jc.bernack@gmail.com>
2023-03-02 18:47:07 +0100

    move binaries to resources directory

--- libevm/build.sh
+++ libevm/build.sh
@@ -2,11 +2,15 @@
 
 set -ex
 
+resourcesDir=../evm/src/main/resources
+linuxPrefix=linux-x86-64
+windowsPrefix=win32-x86-64
+
 # delete output directory
 rm -rf bin
 
 echo "# building for linux"
-go build -buildmode c-shared -o "bin/linux-x86-64/libevm.so"
+go build -buildmode c-shared -o "bin/$linuxPrefix/libevm.so"
 
 echo "# building for windows"
 GOOS=windows \
@@ -14,17 +18,19 @@ GOARCH=amd64 \
 CGO_ENABLED=1 \
 CC=x86_64-w64-mingw32-gcc \
 CXX=x86_64-w64-mingw32-g++ \
-go build -buildmode c-shared -o bin/win32-x86-64/libevm.dll
+go build -buildmode c-shared -o bin/$windowsPrefix/libevm.dll
 
 # alternative: build using docker container
-#docker build -t horizen/libevm:win32-x86-64 .
-#docker run --rm -v "$PWD":/libevm -w /libevm horizen/libevm:win32-x86-64 /bin/sh -c \
-#  "go build -buildmode c-shared -o bin/win32-x86-64/libevm.dll"
+#docker build -t horizen/libevm:$windowsPrefix .
+#docker run --rm -v "$PWD":/libevm -w /libevm horizen/libevm:$windowsPrefix /bin/sh -c \
+#  "go build -buildmode c-shared -o bin/$windowsPrefix/libevm.dll"
 
 # dump symbols of shared object
-#nm -g bin/linux-x86-64/libevm.so
-#winedump -j export bin/win32-x86-64/libevm.dll
+#nm -g bin/$linuxPrefix/libevm.so
+#winedump -j export bin/$windowsPrefix/libevm.dll
 
 echo "# copy binaries to the EVM package"
-cp -r bin/linux-x86-64 ../evm/target/classes/
-cp -r bin/win32-x86-64 ../evm/target/classes/
+mkdir -p $resourcesDir/$linuxPrefix
+cp bin/$linuxPrefix/libevm.so $resourcesDir/$linuxPrefix/libevm.so
+mkdir -p $resourcesDir/$windowsPrefix
+cp bin/$windowsPrefix/libevm.dll $resourcesDir/$windowsPrefix/libevm.dll
