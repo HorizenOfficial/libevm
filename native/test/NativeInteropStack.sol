204caca2a0029724d8e6758e73cbfdb2042d7d5c
Jan Christoph Bernack <jc.bernack@gmail.com>
2023-06-27 19:35:49 +0200

    cleanup

--- native/test/NativeInteropStack.sol
+++ native/test/NativeInteropStack.sol
@@ -2,9 +2,9 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract NativeInteropStackContract {
+contract NativeInteropStack {
     function loop(address target, uint32 counter) public returns (uint32) {
-        try NativeInteropStackContract(target).loop(address(this), counter + 1) returns (uint32 returnedCounter) {
+        try NativeInteropStack(target).loop(address(this), counter + 1) returns (uint32 returnedCounter) {
             return returnedCounter;
         } catch {
             return counter;
